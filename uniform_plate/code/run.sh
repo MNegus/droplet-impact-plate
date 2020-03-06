40419f2074b9ec895c1229e21b52688e79f294ae
MNegus <michaelnegus1@gmail.com>
2020-03-06 10:19:09 +0000

    Edited code run

--- uniform_plate/code/run.sh
+++ uniform_plate/code/run.sh
@@ -2,7 +2,7 @@
 
 export OMP_NUM_THREADS=8
 
-for PLATE_VEL in 0.2 0.3 0.4 0.5
+for PLATE_VEL in 0 0.02 0.04 0.06 0.08 0.1
 do
     # Deletes the previous directory and test files
     rm -r uniform_plate
@@ -14,7 +14,7 @@ do
 
     make uniform_plate.tst PLATE_VEL=-$PLATE_VEL
 
-    export DATA_DIRECTORY=plate_vel_${PLATE_VEL}
+    export DATA_DIRECTORY=/scratch/negus/uniform_plate/plate_vel_${PLATE_VEL}
 
     rm -r $DATA_DIRECTORY
     mv uniform_plate $DATA_DIRECTORY
