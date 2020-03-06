1079deb0c61359ed82b9275e6c23d75f5f6f4a2d
MNegus <michaelnegus1@gmail.com>
2020-03-06 10:18:34 +0000

    (Rushed) update for analysis and cleaning

--- uniform_plate/data_analysis/data_analysis.m
+++ uniform_plate/data_analysis/data_analysis.m
@@ -15,48 +15,44 @@ addpath("~/repos/plate-impact/data_analysis/interface_analysis");
 master_directory = '/scratch/Uniform_plate/cleaned_data/';
 
 % Names of the individual directories where the data is stored
-data_directories = ["uniform_plate"];
-
-% Edits so the full directory address is given in data_directories
-for k = 1:length(data_directories)
-    data_directories(k) = strcat(master_directory, data_directories(k));
-end
+plate_velocity= 0.1;
+data_directory = sprintf("/scratch/Uniform_plate/cleaned_data/plate_vel_%.1f", plate_velocity)
 
 % Readable names to label the plots for each of the data directories
-legend_entries = ["Plate"];
+legend_entry = sprintf("Plate velocity = %.1f", plate_velocity);
 
 %% Volume conservation
 % Plots the volume of the droplet volume fraction as a function of time for
 % all of the simulation to check how well volume (or equivalently mass) is
 % conserved
-
-initial_vol = 4 * pi / 3; % Initial volume of the droplet
-
-% Sets up figure
-close(figure(1));
-figure(1);
-hold on;
-grid on;
-xlabel("$t$", "Interpreter", "latex", 'Fontsize', 15);
-ylabel("Volume / Initial volume", "Interpreter", "latex", 'Fontsize', 15);
-ax = gca;
-ax.FontSize = 12;
-set(gca,'TickLabelInterpreter','latex');
-title("Volume conservation of droplet phase", "Interpreter", "latex", ...
-    'Fontsize', 15);
-
-% Iterates through the data directories. The "volumes.txt" files contain
-% two columns, the first is time and the second is volume
-for k = 1:length(data_directories)
-    data_matrix = dlmread(strcat(data_directories(k), '/volumes.txt'));
-    plot(data_matrix(:, 1), data_matrix(:, 2) / initial_vol);
-end
-% Sets up the legend
-L = legend(legend_entries);
-set(L, 'Interpreter', 'latex');
-set(L, 'FontSize', 11);
-set(L, 'Location', 'northwest');
-print(gcf,'Figures/volume.png','-dpng','-r300');
+% 
+% initial_vol = 4 * pi / 3; % Initial volume of the droplet
+% 
+% % Sets up figure
+% close(figure(1));
+% figure(1);
+% hold on;
+% grid on;
+% xlabel("$t$", "Interpreter", "latex", 'Fontsize', 15);
+% ylabel("Volume / Initial volume", "Interpreter", "latex", 'Fontsize', 15);
+% ax = gca;
+% ax.FontSize = 12;
+% set(gca,'TickLabelInterpreter','latex');
+% title("Volume conservation of droplet phase", "Interpreter", "latex", ...
+%     'Fontsize', 15);
+% 
+% % Iterates through the data directories. The "volumes.txt" files contain
+% % two columns, the first is time and the second is volume
+% for k = 1:length(data_directories)
+%     data_matrix = dlmread(strcat(data_directories(k), '/volumes.txt'));
+%     plot(data_matrix(:, 1), data_matrix(:, 2) / initial_vol);
+% end
+% % Sets up the legend
+% L = legend(legend_entries);
+% set(L, 'Interpreter', 'latex');
+% set(L, 'FontSize', 11);
+% set(L, 'Location', 'northwest');
+% print(gcf,'Figures/volume.png','-dpng','-r300');
 
 %% Pressure along plate
 % Creates an animation of the pressure along the plate in time. We can vary
@@ -69,66 +65,59 @@ print(gcf,'Figures/volume.png','-dpng','-r300');
 % Turnover point
 d = @(t) sqrt(3 * t);
 
-% Wagner pressure
-wagner_p = @(t, r) 3 ./ (pi * sqrt(3 * t - r.^2));
-
 % Impact time
-impact_time = 0.203;
-
-h = 0; % Number of cells above plate
-
-% Sets up figure
-close(figure(2));
-figure(2);
-hold on;
-grid on;
-xlabel("$r$", "Interpreter", "latex", 'Fontsize',30);
-ylabel("Pressure, $p$", "Interpreter", "latex", 'Fontsize', 30);
-ax = gca;
-ax.FontSize = 16;
-set(gca,'TickLabelInterpreter','latex');
-x_limits = [0, 1];
-xlim(x_limits);
-ylim([0 15]);
-
-% Reads the "times.txt" file from the first data directory. In theory this
-% should be identical in all the cases
-times = dlmread(strcat(data_directories(1), '/plate_outputs/times.txt'));
-
-% Create animated lines and place them in a matrix
-line1 = animatedline('Color', [0    0.4470    0.7410]);
-line2 = animatedline('Color', [0.8500    0.3250    0.0980]);
-line3 = animatedline('Color', [0.9290    0.6940    0.1250]);
-line4 = animatedline('Color', [0.4940    0.1840    0.5560]);
-animlines = [line1, line2, line3, line4];
-
-wagner_line = animatedline('Color', [0.4660    0.6740    0.1880]);
+impact_time = (2.125 - 1 - 1) / (1 - plate_velocity)
 
 
- % Sets up the legend
-dir_arr = [1];
-L = legend([legend_entries(dir_arr), 'Wagner']);
-set(L, 'Interpreter', 'latex');
-set(L, 'FontSize', 15);
+for h = [0, 1, 2]
+    % Sets up figure
+    close(figure(2));
+    figure(2);
+    hold on;
+    grid on;
+    xlabel("$r$", "Interpreter", "latex", 'Fontsize',30);
+    ylabel("Pressure, $p$", "Interpreter", "latex", 'Fontsize', 30);
+    ax = gca;
+    ax.FontSize = 16;
+    set(gca,'TickLabelInterpreter','latex');
+    x_limits = [0, 1];
+    xlim(x_limits);
+    ylim([0 15]);
 
-width=800;
-height=800;
-set(gcf,'position',[10,10,width,height])
-% create the video writer with 1 fps
-writerObj = VideoWriter(sprintf('Videos/pressure_%d.avi', h));
-writerObj.FrameRate = 5;
-open(writerObj);
-% Iterates over all times
-for m = 205:750
-    % Choses which data directories to show
-    
-    t = 0.001 * m; % Time
-    
-    for k = dir_arr
+    % Reads the "times.txt" file from the first data directory. In theory this
+    % should be identical in all the cases
+    times = dlmread(strcat(data_directory, '/plate_outputs/times.txt'));
+
+    % Create animated lines and place them in a matrix
+    animline = animatedline('Color', [0    0.4470    0.7410]);
+
+    wagner_line = animatedline('Color', [0.8500    0.3250    0.0980]);
+
+
+     % Sets up the legend
+    L = legend([legend_entry, 'Wagner']);
+    set(L, 'Interpreter', 'latex');
+    set(L, 'FontSize', 15);
+
+    width=800;
+    height=800;
+    set(gcf,'position',[10,10,width,height])
+    % create the video writer with 1 fps
+    writerObj = VideoWriter(sprintf('Videos/pressure_h_%d_vel_%.1f.avi', h, plate_velocity));
+    writerObj.FrameRate = 5;
+    open(writerObj);
+
+    % Iterates over all times
+    start_pos = floor(0.9 * impact_time * 1000);
+    for m = start_pos: start_pos + 100
+        % Choses which data directories to show
+
+        t = 0.001 * m; % Time
+
         % Loads in data from the text file
-        output_matrix = dlmread(strcat(data_directories(k), ...
+        output_matrix = dlmread(strcat(data_directory, ...
             '/plate_outputs/output_', num2str(m), '.txt'));
-    
+
         % Isolates data with matching value of h
         h_column = output_matrix(:, 2); 
         refined_mat = output_matrix(h_column == h, :);
@@ -138,29 +127,36 @@ for m = 205:750
         sorted_mat = refined_mat(sorted_idxs, :);
 
         % Creates the animated line
-        clearpoints(animlines(k))
-        addpoints(animlines(k), sorted_mat(:, 1), sorted_mat(:, 3));
+        clearpoints(animline)
+        addpoints(animline, sorted_mat(:, 1), sorted_mat(:, 3));
+
+        % Wagner line
+        clearpoints(wagner_line);
+        if t > impact_time
+            sigmas =  10.^linspace(-10, 5, 1e4);
+            [wagner_rs, wagner_ps] = wagner_pressure(sigmas, t - impact_time, plate_velocity, 1);
+            addpoints(wagner_line, wagner_rs, wagner_ps);
+        end
+
+
+        ylim([0 , 20]);
+        xlim([0, max([1, 1.5 * d(t - impact_time)])]);
+
+        title(sprintf("Pressure: $h$ = %d, $t$ = %g, Plate vel = %g\n", h, times(m, 2), plate_velocity), ...
+            "Interpreter", "latex", 'Fontsize', 15);
+        drawnow;
+        frame = getframe(gcf);
+        writeVideo(writerObj, frame);
     end
-   
-    % Wagner line
-    clearpoints(wagner_line);
-    if t > impact_time
-        wagner_rs = linspace(x_limits(1), 0.99 * d(t - impact_time), 1e3);
-        wagner_vals = wagner_p(t - impact_time, wagner_rs);
-        addpoints(wagner_line, wagner_rs, wagner_p(t - impact_time, ...
-            wagner_rs));
-    end
-    ylim([0 ,max([max(wagner_vals), 10])]);
-    xlim([0, max([1, 1.5 * d(t - impact_time)])]);
+
+    close(writerObj);
     
-    title(sprintf("Pressure: $h$ = %d, $t$ = %g", h, times(m, 2)), ...
-        "Interpreter", "latex", 'Fontsize', 15);
-    drawnow;
-    frame = getframe(gcf);
-    writeVideo(writerObj, frame);
 end
 
-close(writerObj);
+
+
+
+
 
 %%
 %
--- uniform_plate/data_cleaning/data_clean.sh
+++ uniform_plate/data_cleaning/data_clean.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
+RAWDATADIRECTORY=/scratch/Uniform_plate/raw_data
 
-
-
-RAWDATADIRECTORY=~/repos/plate-impact/uniform_plate/code/
-coderun=uniform_plate
-CLEANEDDIRECTORY=/scratch/Uniform_plate/cleaned_data/$coderun
-
-source ~/repos/plate-impact/post_processing/data_cleaning/simulation_output_clean.sh $RAWDATADIRECTORY $coderun $CLEANEDDIRECTORY
+for coderun in plate_vel_0.1 plate_vel_0.2 plate_vel_0.3 plate_vel_0.4 plate_vel_0.5
+do
+    CLEANEDDIRECTORY=/scratch/Uniform_plate/cleaned_data/$coderun
+    source ~/repos/plate-impact/post_processing/data_cleaning/simulation_output_clean.sh $RAWDATADIRECTORY $coderun $CLEANEDDIRECTORY
+done
 
