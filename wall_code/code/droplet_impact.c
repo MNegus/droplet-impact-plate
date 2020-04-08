03688d8e0402d615e82aa6de897a4c3d0ff2edc0
MNegus <michaelnegus1@gmail.com>
2020-04-08 15:51:16 +0100

    Updating wall code to fit with the format of the plate codes

--- wall_code/code/droplet_impact.c
+++ wall_code/code/droplet_impact.c
@@ -15,26 +15,23 @@
 #include "tag.h" // For removing small droplets
 #include <omp.h> // For openMP parallel
 
-/* Physical constants */
-double DROP_CENTRE; // Initial position of the centre of the droplet
 
-/* Computational constants */
-double DROP_REFINED_WIDTH; // Width of refined areas around droplet
-double REFINED_BOX_HEIGHT; // Height of the refined areas around substrate
-double REFINED_BOX_WIDTH; // Width of the refined area around the substrate
-double MIN_CELL_SIZE; // Size of the cells at the most refined
+/* Computational constants derived from parameters */
+double MIN_CELL_SIZE; // Size of the smallest cell
+double DROP_REFINED_WIDTH; // Width of the refined area around the droplet
+double PLATE_REFINED_WIDTH; // Width of the refined area around the plate
+double IMPACT_TIME; // Theoretical time of impact
+double MAX_TIME; // Maximum time to run the simulation for
+double INTERPOLATE_DISTANCE; // Distance above plate to read the pressure
 
-/* Outputting constants */
-int gfs_output_no = 1; // Used to count how many gfsview outputs there have been
-int interface_output_no = 1; // How many interface outputs there have been
-int pressure_output_no = 1; // How many pressure outputs there have been
-// Stores times the interface was outputted
-char interface_time_filename[] = "interface_times.txt"; 
-char droplet_area_filename[] = "area.txt";
-
+/* Global variables */
+int gfs_output_no = 1; // Records how many GFS files have been outputted
+int plate_output_no = 1; // Records how many plate data files there have been
+int interface_output_no = 1; // Records how many interface files there have been
+double start_wall_time; // Time the simulation was started
+double end_wall_time; // Time the simulation finished
 
 /* Boundary conditions */
-
 // Conditions for entry from above
 u.n[right] = neumann(0.); // Allows outflow through boundary
 p[right] = dirichlet(0.); // 0 pressure far from surface
@@ -50,15 +47,8 @@ u.t[left] = dirichlet(0.); // No slip at surface
 
 int main() {
 
-    /* Derived constants */
-    DROP_CENTRE = INTITIAL_DROP_HEIGHT + DROP_RADIUS; // Centre of droplet
-    DROP_REFINED_WIDTH = 0.1 * DROP_RADIUS; // Width of refined around droplet
-    REFINED_BOX_HEIGHT = 0.02 * DROP_RADIUS; // Height of refined box around wall
-    REFINED_BOX_WIDTH = BOX_WIDTH / 4.; // Width of refined box around wall
-    MIN_CELL_SIZE = BOX_WIDTH / pow(2, MAXLEVEL); // Size of the smallest cell
-
+    /* Create the computational domain */
     init_grid(1 << MINLEVEL); // Create grid according to the minimum level
-
     size(BOX_WIDTH); // Size of the domain
 
     /* Set physical constants */
@@ -68,6 +58,17 @@ int main() {
     mu2 = mu1 * MU_R; // Viscosity of air phase
     f.sigma = 1. / WEBER; // Surface tension at interface
 
+    /* Derived constants */
+    MIN_CELL_SIZE = BOX_WIDTH / pow(2, MAXLEVEL); // Size of the smallest cell
+    PLATE_REFINED_WIDTH = 0.3 * PLATE_THICKNESS; // Refined region around plate
+    DROP_REFINED_WIDTH = 0.05; // Refined region around droplet
+    IMPACT_TIME = (DROP_CENTRE - DROP_RADIUS) / (-DROP_VEL);
+    INTERPOLATE_DISTANCE = MIN_CELL_SIZE; // Distance above plate to read pressure
+
+    /* Maximum time is shortly after Wagner theory would predict the turnover 
+    point reaches the radius of the droplet */
+    double wagner_max_time = 1.5 * (IMPACT_TIME + 1. / 3.);
+    MAX_TIME = min(HARD_MAX_TIME, wagner_max_time);
     run(); // Runs the simulation
 }
 
@@ -75,150 +76,149 @@ int main() {
 event init(t = 0) {
 /* Initialises the flow as a spherical droplet falling downwards */
 
-    /* Initial refinement */
-    // Refines region around the droplet
+    // Records the wall time
+    start_wall_time = omp_get_wtime();
+
+    /* Refines around the droplet */
     refine(sq(x - DROP_CENTRE) + sq(y) < sq(DROP_RADIUS + DROP_REFINED_WIDTH) \
-        && sq(x - DROP_CENTRE) + sq(y) > sq (DROP_RADIUS - DROP_REFINED_WIDTH) \
+        && sq(x - DROP_CENTRE) + sq(y) > sq(DROP_RADIUS - DROP_REFINED_WIDTH) \
         && level < MAXLEVEL);
-
-    // Refine around the substrate
-    refine(x < REFINED_BOX_HEIGHT && y < REFINED_BOX_WIDTH && level < MAXLEVEL);
-
-    /* Initialise droplet volume fraction */ 
-    fraction(f, - sq(x - DROP_CENTRE) - sq(y) + sq(DROP_RADIUS)); 
     
+    /* Initialises the droplet volume fraction */
+    fraction(f, -sq(x - DROP_CENTRE) - sq(y) + sq(DROP_RADIUS));
 
     /* Initialise the droplet velocity downwards */
     foreach() {
-        u.x[] = -f[];
+        u.x[] = DROP_VEL * f[];
     }
-    
-     /* Initialise the file for outputting the times at which interfaces are 
-        saved */
-    FILE *interface_time_file = fopen(interface_time_filename, "w");
-    fclose(interface_time_file);
-}
-
-
-event acceleration (i++) {
-/* Adds acceleration due to gravity at each time step */
-    face vector av = a; // Acceleration at each face
-    foreach_face(x) av.x[] -= 1./sq(FR); // Adds acceleration due to gravity
 }
 
 
 event refinement (i++) {
 /* Refines the grid where appropriate */
 
-    // Adapt the mesh for rapid changes in the free surface, velocity and 
-    // vorticity.
+    /* Adapts with respect to velocities and volume fraction */
     adapt_wavelet ({u.x, u.y, f}, (double[]){1e-2, 1e-2, 1e-2},
-        maxlevel = MAXLEVEL, minlevel = MINLEVEL);
+        minlevel = MINLEVEL, maxlevel = MAXLEVEL);
     
-    // Refine around the substrate
-    refine(x < REFINED_BOX_HEIGHT && y < REFINED_BOX_WIDTH && level < MAXLEVEL);
+    /* Refines above the plate */
+    refine((y < PLATE_WIDTH) && (x < 0.5 * PLATE_REFINED_WIDTH) \
+        && level < MAXLEVEL);
 }
 
 
-// event small_droplet_removal (i++) {
-// /* Removes any small droplets that have formed, that are smaller than a specific    
-//     size */
-//     remove_droplets(f, 20); // Removes droplets of diameter 6 cells or less
-// }
+event gravity (i++) {
+/* Adds acceleration due to gravity at each time step */
+    face vector av = a; // Acceleration at each face
+    foreach_face(x) av.x[] -= 1./sq(FR); // Adds acceleration due to gravity
+}
 
 
-event pressure_output (t += 0.01) {
-/* Outputs the pressure along the surface of the plate */
+event small_droplet_removal (i++) {
+/* Removes any small droplets or bubbles that have formed, that are smaller than
+    a specific size */
+    // Removes droplets of diameter 5 cells or less
+    remove_droplets(f, 5);
 
-    // Creates and opens the file to output pressure into
-    char pressure_filename[80];
-    sprintf(pressure_filename, "pressure_%d.txt", pressure_output_no);
-    FILE *pressure_file = fopen(pressure_filename, "w");
+    // Removes bubbles of diameter 5 cells or less
+    remove_droplets(f, 5, 1e-4, true); 
+}
 
-    foreach() {
-        // If x < MIN_CELL_SIZE, then we are currently at the cell directly 
-        // above the wall
-        if (x < MIN_CELL_SIZE ) {
-            // Outputs the value of pressure to the file
-            fprintf(pressure_file, "%g %g %g\n", y, x, p[]);
+
+event output_volume (t += 0.001) {
+/* Outputs the volume of the liquid phase to the log file */
+    fprintf(stderr, "t = %g, volume = %g\n", t, 2 * pi * statsf(f).sum);
+}
+
+
+event output_interface (t += INTERFACE_OUTPUT_TIMESTEP) {
+/* Outputs the interface locations of the droplet */
+    if ((t >= START_OUTPUT_TIME) && (t <= END_OUTPUT_TIME)) {
+    // Creates text file to save output to
+    char interface_filename[80];
+    sprintf(interface_filename, "interface_%d.txt", interface_output_no);
+    FILE *interface_file = fopen(interface_filename, "w");
+
+    // Outputs the interface locations and closes the file
+    output_facets(f, interface_file);
+    fclose(interface_file);
+
+    interface_output_no++;
+    }
+}
+
+
+event output_values_along_plate (t += PLATE_OUTPUT_TIMESTEP) {
+/* Outputs the values of pressure along the plate */
+    if ((t >= START_OUTPUT_TIME) && (t <= END_OUTPUT_TIME)) {
+    // Creates the file for outputting data
+    char plate_output_filename[80];
+    sprintf(plate_output_filename, "plate_output_%d.txt", plate_output_no);
+    FILE *plate_output_file = fopen(plate_output_filename, "w");
+
+    // Adds the time to the first line of the file
+    fprintf(plate_output_file, "t = %g\n", t);
+
+    foreach_boundary(left) {
+        if (y < PLATE_WIDTH) {
+            fprintf(plate_output_file, \
+            "y = %g, x = %g, p = %g\n", y, x, p[1, 0]);
         }
     }
 
-    fclose(pressure_file); // Closes the pressure file
+    fclose(plate_output_file);
 
-    pressure_output_no++; // Increments the number of outputs there have been
-}
-
-event images (t += 0.01; t <= 2.) {
-/* Produces movies and images using bview */
-
-    // Voriticty calculation
-    scalar omega[];
-    vorticity (u, omega);
-
-    // Set up bview box
-    view (width = 512, height = 512, fov = 20, ty = -0.5, quat = {0, 0, -0.707, 0.707});
-
-    // Movie of the volume fraction of the droplet
-    clear();
-    draw_vof("f", lw = 2);
-    squares("f", linear = true);
-    mirror ({0,1}) {
-        draw_vof("f", lw = 2);
-        squares("f", linear = true);
+    plate_output_no++; // Increments output number
     }
-    save ("tracer.mp4");
-
-    // Movie of the vorticity
-    clear();
-    squares("omega", linear = true);
-    draw_vof("f", lw = 2);
-    mirror ({0,1}) {
-        draw_vof("f", lw = 2);
-        squares("omega", linear = true);
-    }
-    save ("vort.mp4");
 }
 
 
-event gfs_output (t += 0.01) {
-    // Saves a gfs file
-
+event gfs_output (t += GFS_OUTPUT_TIMESTEP) {
+/* Saves a gfs file */
+    if ((t >= START_OUTPUT_TIME) && (t <= END_OUTPUT_TIME)) {
     char gfs_filename[80];
     sprintf(gfs_filename, "gfs_output_%d.gfs", gfs_output_no);
     output_gfs(file = gfs_filename);
 
     gfs_output_no++;
+    }
 }
 
 
-event interface_output (t += 0.01) {
-/* Outputs the location of the interfaces of the droplet and the plate */
+// event images (t += 0.01; t <= 2.) {
+// /* Produces movies and images using bview */
 
-    /* Filenames for the outputs */
-    char droplet_interface_filename[80];
-    sprintf(droplet_interface_filename, \
-        "droplet_interface_%d.txt", interface_output_no);
-    FILE *droplet_interface_file = fopen(droplet_interface_filename, "w");
+//     // Voriticty calculation
+//     scalar omega[];
+//     vorticity (u, omega);
 
-    
-    /* Appends the timestep number and the time for this specific output_no, so 
-    that in post-processing we know what these are for each interface file */
-    FILE *interface_time_file = fopen(interface_time_filename, "a");
-    fprintf(interface_time_file, "%d %d %g\n", interface_output_no, i, t); 
-    fclose(interface_time_file);
+//     // Set up bview box
+//     view (width = 512, height = 512, fov = 20, ty = -0.5, quat = {0, 0, -0.707, 0.707});
 
-    /* Outputs the interface locations */
-    output_facets(f, droplet_interface_file); // Droplet output
-    fclose(droplet_interface_file);
+//     // Movie of the volume fraction of the droplet
+//     clear();
+//     draw_vof("f", lw = 2);
+//     squares("f", linear = true);
+//     mirror ({0,1}) {
+//         draw_vof("f", lw = 2);
+//         squares("f", linear = true);
+//     }
+//     save ("tracer.mp4");
 
-    /* Outputs the area of the droplet */
-    FILE *area_file = fopen(droplet_area_filename, "a");
-    fprintf(area_file, "%g %g\n", t, 2 * pi * statsf(f).sum);
-    fclose(area_file);
+//     // Movie of the vorticity
+//     clear();
+//     squares("omega", linear = true);
+//     draw_vof("f", lw = 2);
+//     mirror ({0,1}) {
+//         draw_vof("f", lw = 2);
+//         squares("omega", linear = true);
+//     }
+//     save ("vort.mp4");
+// }
 
-    interface_output_no++; // Increments number of outputs
+
+event end (t = MAX_TIME) {
+    end_wall_time = omp_get_wtime();
+    fprintf(stderr, "Finished after %g seconds\n", end_wall_time - start_wall_time);
 }
-
-
 
--- wall_code/code/parameters.h
+++ wall_code/code/parameters.h
@@ -9,10 +9,17 @@ const double WEBER = 10000.0; // Weber number
 const double FR = 10.1; // Froude number
 const double DROP_VEL = -1.0; // Initial velocity of the droplet 
 const double DROP_RADIUS = 1.0; // Radius of droplet
-const double INTITIAL_DROP_HEIGHT = 0.56; // Distance of drop from surface
+const double DROP_CENTRE = 1.125; // Initial centre position of droplet
+const double PLATE_WIDTH = 3.0; // Width of plate (horizontal direction)
+const double PLATE_THICKNESS = 0.15; // Thickness (vertical direction)
 
 /* Computational constants */
 const int MINLEVEL = 4; // Minimum refinement level 
-const int MAXLEVEL = 10; // Maximum refinement level
-const double BOX_WIDTH = 7.5; // Width of the computational box
-
+const int MAXLEVEL = 12; // Maximum refinement level
+const double BOX_WIDTH = 4.0; // Width of the computational box
+const double START_OUTPUT_TIME = 0.0; // Time to start outputs
+const double END_OUTPUT_TIME = 2.0; // Time to end outputs
+const double GFS_OUTPUT_TIMESTEP = 1e-2; // Time between gfs outputs
+const double PLATE_OUTPUT_TIMESTEP = 1e-3; // Time between plate outputs
+const double INTERFACE_OUTPUT_TIMESTEP = 1e-3; // Time between interface outputs
+const double HARD_MAX_TIME = 2.0; // Hard maximum time 
