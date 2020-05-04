b890bfcb3e634287687eac2c94ae502d124f1903
MNegus <michaelnegus1@gmail.com>
2020-05-04 11:04:40 +0100

    Removing validation scripts from the repository

--- force_test/README.md
+++
@@ -1,4 +0,0 @@
-# force_test
-
-Tests the force integration by presenting a simpler problem with a known 
-analytical solution
--- force_test/code/parameters.h
+++
@@ -1,7 +0,0 @@
-/* parameters.h
-Header file for the force test to feed in relevant parameters */
-
-const int LEVEL = 7; // Refinement level of the grid
-const double BOX_WIDTH = 4.; // Width of the computational box
-const double MAX_TIME = 1.; // Maximum time to run the simulation for
-const double TIMESTEP = 1e-3; // Timestep between force calculations
