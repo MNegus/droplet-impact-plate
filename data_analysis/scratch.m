5696b080a2cd160ebb0c93f05799be5ae780966b
Michael <michaelnegus1@gmail.com>
2021-01-06 12:02:11 +0000

    General changes

--- data_analysis/scratch.m
+++ data_analysis/scratch.m
@@ -1,117 +1,49 @@
-% addpath("forces");
-% 
-% eps = 1;
-% t_max = 1.0;
-% alpha = 2;
-% beta = 0;
-% gamma = 100;
-% 
-% beta_critical = 2 * sqrt(alpha * gamma);
-% 
-% close all;
-% 
-% figure(1);
-% hold on;
-% % 
-% % figure(2);
-% % hold on;
-% % 
-% % figure(3);
-% % hold on;
-% % 
-% % figure(4);
-% % hold on;
-% for beta = [0, 0.25 * beta_critical, 0.5 * beta_critical, beta_critical, 5.0 * beta_critical]
-% % for beta = [0, 0.5 * beta_critical, beta_critical, 2.0 * beta_critical]
-%    [t, s, sdot, sddot] = s_solution(t_max, alpha, beta, gamma, eps);
-%    Fs = composite_force(t, s, sdot, sddot, eps);
-%    figure(1);
-%    plot(t, Fs);
-%    
-% %    figure(2);
-% %    plot(t, s);
-% %    
-% %    figure(3);
-% %    plot(t, sdot);
-% %    
-% %    figure(4);
-% %    plot(t, sddot);
-% end
-% 
-% % % Stationary
-% % s = zeros(size(t));
-% % sdot = zeros(size(t));
-% % sddot = zeros(size(t));
-% % Fs = composite_force(t, s, sdot, sddot, eps);
-% % figure(1);
-% % plot(t, Fs);
-% 
-% 
-% figure(1);
-% legend("0", "0.25", "0.5", "1", "5", "Stationary");
-% 
-% figure(2);
-% legend("0", "0.1", "0.5", "1", "2", "Stationary");
-% 
-% figure(3);
-% legend("0", "0.1", "0.5", "1", "2", "Stationary");
-% 
-% figure(4);
-% legend("0", "0.1", "0.5", "1", "2", "Stationary");
-
-%%
 addpath("forces");
 
-eps = 1;
-t_max = 0.8;
+% Parameters
 alpha = 2;
 beta = 0;
+gamma = 0;
+tmax = 0.2;
+t0 = 1e-9
 
+%% epsilon = 1
+eps = 1;
+tvals = linspace(t0, tmax + t0, 1e3);
+% Solves for s
+[tvals, s, sdot, sddot] = s_solution_alt(tvals, alpha, beta, gamma, eps)
+
+% Determine forces
+outer_F = outer_force(tvals, s, sdot, sddot, eps);
+inner_F = inner_force(tvals, s, sdot, sddot, eps);
+overlap_F = overlap_force(tvals, s, sdot, sddot, eps);
+composite_F = composite_force(tvals, s, sdot, sddot, eps);
+
+%% epsilon = 0.1
+% eps = 0.1;
+% scaled_tvals = linspace(t0, tmax + t0, 1e3) / eps^2;
+% scaled_alpha = alpha / eps^2;
+% scaled_beta = beta;
+% scaled_gamma = eps^2 * gamma;
+% % Solves for s
+% [scaled_tvals, s_small, sdot_small, sddot_small] = s_solution_alt(scaled_tvals, scaled_alpha, scaled_beta, scaled_gamma, eps);
+% t_small = eps^2 * t;
+% 
+% % Determine forces
+% outer_F_small = outer_force(scaled_tvals, s_small, sdot_small, sddot_small, eps);
+% inner_F_small = inner_force(scaled_tvals, s_small, sdot_small, sddot_small, eps);
+% overlap_F_small = overlap_force(scaled_tvals, s_small, sdot_small, sddot_small, eps);
+% composite_F_small = composite_force(scaled_tvals, s_small, sdot_small, sddot_small, eps);
+
+%% Plots comparison
 close all;
-
 figure(1);
 hold on;
-
-for gamma = [0, 10, 50, 100, 500, 1000]
-   [t, s, sdot, sddot] = s_solution(t_max, alpha, beta, gamma, eps);
-   Fs = composite_force(t, s, sdot, sddot, eps);
-   figure(1);
-   plot(t, Fs);
-end
-
-% % Stationary
-% s = zeros(size(t));
-% sdot = zeros(size(t));
-% sddot = zeros(size(t));
-% Fs = composite_force(t, s, sdot, sddot, eps);
-% figure(1);
-% plot(t, Fs);
+plot(tvals, s);
+% plot(eps^2 * scaled_tvals, eps^2 * s_small);
+% plot(tvals, outer_F);
+% plot(scaled_tvals, outer_F_small);
 
 
-figure(1);
-% legend("0", "10", "100", "1000");
-%%
-% 
-% addpath("forces");
-% 
-% eps = 1;
-% t_max = 1.0;
-% alpha = 1;
-% beta = 0;
-% gamma = 50;
-% 
-% [t, s, sdot, sddot] = s_solution(t_max, alpha, beta, gamma, eps);
-% Fs = composite_force(t, s, sdot, sddot, eps);
-% 
-% %%
-% figure(1);
-% plot(t, Fs);
-% 
-% figure(2);
-% plot(t, s);
-% 
-% figure(3);
-% plot(t, sdot);
-% 
-% figure(4);
-% plot(t, sddot);
+figure(2);
+
--- utility_scripts/paper_output_clean.sh
+++ utility_scripts/paper_output_clean.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_2
-./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_5
-./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_10
-./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_20
-./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_100
+# ./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_2
+# ./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_5
+# ./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_10
+# ./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_20
+# ./output_clean.sh /mnt/newarre/cantilever_paper_data/alpha_varying/alpha_100
 
 
-
+for LEVEL in 10 11 12 13 14
+do  
+    ./output_clean.sh /media/michael/newarre/supplementary_material/validation/stationary_validation/level_$LEVEL
+    ./output_clean.sh /media/michael/newarre/supplementary_material/validation/moving_validation/level_$LEVEL
+done
