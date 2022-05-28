4dc79e2c35863454d6b08f7e3ff5a97f0014b9fd
MNegus <michaelnegus1@gmail.com>
2022-05-28 20:50:19 +0100

    Optional core number flag

--- utility_scripts/loop_run.sh
+++ utility_scripts/loop_run.sh
@@ -2,13 +2,13 @@
 
 parent_dir=$1
 code_name=$2
-move_dest=$3
+cores=$3
 
 for MAXLEVEL in 10 11 12 13 14
 do
     echo Max level = $MAXLEVEL
     cd $parent_dir/max_level_$MAXLEVEL/code
 
-    ./run_simulation.sh $code_name 16
+    ./run_simulation.sh $code_name $cores
 done
 
