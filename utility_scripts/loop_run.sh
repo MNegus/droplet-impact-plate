0aeceffbdc54feb83365f38a4059d0d012036500
MNegus <michaelnegus1@gmail.com>
2022-08-02 13:19:02 +0100

    Loop run now runs all files in a directory

--- utility_scripts/loop_run.sh
+++ utility_scripts/loop_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+# Sequentially runs all of the scripts in a given parent directory
 
 parent_dir=$1
 code_name=$2
 cores=$3
 
-for MAXLEVEL in 10 11 12 13 14
+for DIR_NAME in $parent_dir/*/
 do
-    echo Max level = $MAXLEVEL
-    cd $parent_dir/max_level_$MAXLEVEL/code
+    echo $DIR_NAME
+    cd $DIR_NAME/code
 
     ./run_simulation.sh $code_name $cores
 done
-
